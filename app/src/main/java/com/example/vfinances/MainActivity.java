2852d6b24a848f8eb73ab431ce90f626f5b401cd
LuisCruz1027 <luis.cruz04@utrgv.edu>
2023-08-13 15:32:04 -0500

    finished ui for my parts

--- app/src/main/java/com/example/vfinances/MainActivity.java
+++ app/src/main/java/com/example/vfinances/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.vfinances;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.*;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
