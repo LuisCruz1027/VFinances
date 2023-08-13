fc26dcc3b21128be98c8aaa0b3bb97c481ad180e
Francisco Javier Cortez <francisco080304@gmail.com>
2023-08-13 12:56:29 -0500

    My part of the project

---
+++ app/src/main/java/com/example/vfinances/WalletScreen.java
@@ -0,0 +1,14 @@
+package com.example.vfinances;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WalletScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wallet_screen);
+    }
+}
